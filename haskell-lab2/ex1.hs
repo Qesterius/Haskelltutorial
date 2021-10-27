781d5881f4639aa844ba2d98c7891c071522d037
Qesterius <hexeyxd@gmail.com>
2021-10-27 13:53:51 +0200

    nitial

---
+++ haskell-lab2/ex1.hs
@@ -0,0 +1,21 @@
+myFun x = 2*x
+add2T :: Num a =>  (a,a) -> a
+add2T (x,y) = x+y
+
+add2C :: Num a => a -> a -> a
+add2C x y = x+y
+
+add3T :: Num a => (a,a,a) ->a
+add3T (x,y,z) = x+y+z
+
+add3C :: Num a => a -> a -> a ->a
+add3C x y z = x+y+z
+
+
+--curry2 :: ((a,b)->c) -> a -> b ->c
+--curry2 f (x,y) = f x y
+
+fiveToPower_ :: Integer -> Integer
+fiveToPower_ x = 5^x
+
+_ToPower5 :: Num a => a->a
